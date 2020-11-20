3d51bfd6e23c7cfce233bafe18a1e90424c5d15f
Stanley <egnhlab@gmail.com>
2020-11-20 12:23:34 +0800

    Currency Selection event

--- CurrencyConversion/ViewController.swift
+++ CurrencyConversion/ViewController.swift
@@ -11,6 +11,7 @@ import RxSwift
 class ViewController: UIViewController {
     
     @IBOutlet weak var tfCurrency: UITextField?
+    @IBOutlet weak var tfAmount: UITextField?
     
     let currencyPickerView = UIPickerView()
     
@@ -32,18 +33,28 @@ class ViewController: UIViewController {
     }
     
     func setupTFCurrency() {
+        tfCurrency?.placeholder = "Currency"
         tfCurrency?.inputView = currencyPickerView
         tfCurrency?.inputAccessoryView = toolbarForDataPicker(selector: #selector(currencyPickerDoneDidClick))
     }
     
     func binding() {
         viewModel.currencies.bind(to: currencyPickerView.rx.itemTitles) { (row, elements) in
-            return "\(elements.code) \(elements.name)"
+            return "\(elements.code) - \(elements.name)"
+        }.disposed(by: disposeBag)
+        
+        currencyPickerView.rx.itemSelected.subscribe(onNext: { [weak self] value in
+            self?.viewModel.currencyDidSelect(index: value.row)
+        }).disposed(by: disposeBag)
+        
+        viewModel.currencyText.subscribe { [weak self] currencyText in
+            self?.tfCurrency?.text = currencyText
         }.disposed(by: disposeBag)
     }
     
     @objc func currencyPickerDoneDidClick() {
-        
+        tfAmount?.becomeFirstResponder()
+        viewModel.currencyDidSelect(index: currencyPickerView.selectedRow(inComponent: 0))
     }
     
     private func toolbarForDataPicker(selector: Selector?) -> UIToolbar {
--- CurrencyConversion/ViewModel.swift
+++ CurrencyConversion/ViewModel.swift
@@ -11,11 +11,25 @@ import RxCocoa
 class ViewModel {
     
     var currencies = BehaviorRelay<[Currency]>(value: [])
+    var currencyText = BehaviorRelay<String>(value: "")
+    
+    var currencySelected: Currency?
     
     func start() {
         CurrencyService.shared.getCurrencies { [weak self] (currencies) in
             self?.currencies.accept(currencies)
         } failure: { (error) in
+        }
+    }
+    
+    func currencyDidSelect(index: Int) {
+        if index < currencies.value.count {
+            currencySelected = currencies.value[index]
+            if let currencySelected = currencySelected {
+                currencyText.accept("\(currencySelected.code) - \(currencySelected.name)")
+            } else {
+                currencyText.accept("")
+            }
         }
     }
     

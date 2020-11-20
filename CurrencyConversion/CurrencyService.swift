53e41a88981ca0123c7ccb50a58c2203c82acb0f
Stanley <egnhlab@gmail.com>
2020-11-20 14:30:44 +0800

    Code refactoring

--- CurrencyConversion/CurrencyService.swift
+++ CurrencyConversion/CurrencyService.swift
@@ -25,11 +25,11 @@ class CurrencyService {
         session = Session(configuration: urlSessionConfiguration, serverTrustManager: serverTrustManager)
     }
     
-    func getCurrencies(completion: @escaping ([Currency]) -> Void, failure: @escaping (Error?) -> Void) {
-        let url = CurrencyServiceURLBuilder.currencyListURL()
-        let parameters = [
-            "access_key": Config.shared.apiKey()
-        ]
+    private func request(url: String,
+                         parameters: Parameters,
+                         completion: @escaping (NSDictionary) -> Void,
+                         failure: @escaping (Error?) -> Void) {
+        
         session.request(url, parameters: parameters).responseJSON { (response) in
             debugPrint(response)
             switch response.result {
@@ -37,20 +37,7 @@ class CurrencyService {
                 let statusCode = response.response?.statusCode
                 if statusCode == 200 {
                     if let data = data as? NSDictionary {
-                        if data["success"] as? Bool == true {
-                            var result: [Currency] = []
-                            if let currencies = data["currencies"] as? [String: String] {
-                                let keys = currencies.keys.sorted()
-                                for key in keys {
-                                    if let name = currencies[key] {
-                                        result.append(Currency(code: key, name: name))
-                                    }
-                                }
-                            }
-                            completion(result)
-                        } else {
-                            failure(nil)
-                        }
+                        completion(data)
                     } else {
                         failure(nil)
                     }
@@ -60,6 +47,32 @@ class CurrencyService {
             case .failure(_):
                 failure(nil)
             }
+        }
+    }
+    
+    func getCurrencies(completion: @escaping ([Currency]) -> Void, failure: @escaping (Error?) -> Void) {
+        let url = CurrencyServiceURLBuilder.currencyListURL()
+        let parameters = [
+            "access_key": Config.shared.apiKey()
+        ]
+        
+        request(url: url, parameters: parameters) { (data) in
+            if data["success"] as? Bool == true {
+                var result: [Currency] = []
+                if let currencies = data["currencies"] as? [String: String] {
+                    let keys = currencies.keys.sorted()
+                    for key in keys {
+                        if let name = currencies[key] {
+                            result.append(Currency(code: key, name: name))
+                        }
+                    }
+                }
+                completion(result)
+            } else {
+                failure(nil)
+            }
+        } failure: { (error) in
+            failure(nil)
         }
     }
     

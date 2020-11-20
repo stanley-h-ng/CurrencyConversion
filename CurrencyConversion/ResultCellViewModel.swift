86211175a7aeeee228c1ce5e7c02bd9c937a6523
Stanley <egnhlab@gmail.com>
2020-11-20 17:15:44 +0800

    Calculate results

---
+++ CurrencyConversion/ResultCellViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  ResultCellViewModel.swift
+//  CurrencyConversion
+//
+//  Created by Stanley NG on 20/11/2020.
+//
+
+import Foundation
+
+class ResultCellViewModel {
+    
+    var currency = ""
+    var amount = 0.0
+    
+}
--- CurrencyConversion/ViewModel.swift
+++ CurrencyConversion/ViewModel.swift
@@ -12,6 +12,7 @@ class ViewModel {
     
     var currencies = BehaviorRelay<[Currency]>(value: [])
     var currencyText = BehaviorRelay<String>(value: "")
+    var results = BehaviorRelay<[ResultCellViewModel]>(value: [])
     
     var currencySelected: Currency?
     var amount = 0.0
@@ -20,12 +21,8 @@ class ViewModel {
         CurrencyService.shared.getCurrencies { [weak self] (currencies) in
             self?.currencies.accept(currencies)
         } failure: { (error) in
+            // TODO: Show Error
         }
-        
-        CurrencyService.shared.getConversionRates { conversionRates in
-        } failure: { (error) in
-        }
-
     }
     
     func currencyDidSelect(index: Int) {
@@ -36,11 +33,42 @@ class ViewModel {
             } else {
                 currencyText.accept("")
             }
+            calculateResults()
         }
     }
     
     func amountDidUpdate(amount: String) {
         self.amount = Double(amount) ?? 0
+        calculateResults()
     }
     
+    func calculateResults() {
+        if let currencySelected = currencySelected, amount != 0 {
+            CurrencyService.shared.getConversionRates { [weak self] conversionRates in
+                if let self = self {
+                    if let conversionRateToUSD = conversionRates["USD\(currencySelected.code)"] {
+                        let amountInUSD = self.amount / conversionRateToUSD
+                        var resultCellViewModels: [ResultCellViewModel] = []
+                        for curreny in self.currencies.value {
+                            if let desiredConversionRate = conversionRates["USD\(curreny.code)"] {
+                                let resultCellViewModel = ResultCellViewModel()
+                                resultCellViewModel.currency = "\(curreny.code) - \(curreny.name)"
+                                resultCellViewModel.amount = amountInUSD * desiredConversionRate
+                                resultCellViewModels.append(resultCellViewModel)
+                            }
+                        }
+                        self.results.accept(resultCellViewModels)
+                    } else {
+                        // TODO: Show Error
+                        self.results.accept([])
+                    }
+                }
+            } failure: { [weak self] error in
+                // TODO: Show Error
+                self?.results.accept([])
+            }
+        } else {
+            results.accept([])
+        }
+    }
 }
